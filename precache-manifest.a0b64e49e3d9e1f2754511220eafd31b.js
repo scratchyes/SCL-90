acffcf8f127acf8fef31ed4c3c6bd94a0379573a
MCardenas <mcarden001@citymail.cuny.edu>
2019-06-15 12:20:36 -0400

    Updates

---
+++ precache-manifest.a0b64e49e3d9e1f2754511220eafd31b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40202f883719c6aeeea63d692902e74",
+    "url": "/90sclickygame/index.html"
+  },
+  {
+    "revision": "147355835d6a3a46f359",
+    "url": "/90sclickygame/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/90sclickygame/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "147355835d6a3a46f359",
+    "url": "/90sclickygame/static/js/main.8481eefc.chunk.js"
+  },
+  {
+    "revision": "f87eebc38ccf69a8eb7d",
+    "url": "/90sclickygame/static/js/runtime~main.f9f37b73.js"
+  }
+]);
